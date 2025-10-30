404db90e07a488126d03d3bc17493b7dbaece1e4
Ahmed1m0 <ahmed0658484462@gmail.com>
2025-10-30 09:34:15 +0100

    first commit

---
+++ src/main/java/ma/rest/tp8/enums/TypeCompte.java
@@ -0,0 +1,5 @@
+package ma.rest.tp8.enums;
+
+public enum TypeCompte {
+    COURANT, EPARGNE
+}
---
+++ src/main/java/ma/rest/tp8/repositories/CompteRepository.java
@@ -0,0 +1,9 @@
+package ma.rest.tp8.repositories;
+
+import ma.rest.tp8.entities.Compte;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompteRepository extends JpaRepository<Compte, Long> {
+}
